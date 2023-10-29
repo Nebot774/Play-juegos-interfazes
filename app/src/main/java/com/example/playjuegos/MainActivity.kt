bd355a91591158578866760aaf782744e7190c1d
Nebot774 <adriclemenbot@gmail.com>
2023-10-29 21:11:51 +0100

    actividad 2 completa

--- app/src/main/java/com/example/playjuegos/MainActivity.kt
+++ app/src/main/java/com/example/playjuegos/MainActivity.kt
@@ -2,32 +2,38 @@ package com.example.playjuegos
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val jugador=findViewById<Button>(R.id.button2)
-
-        jugador.setOnClickListener{lanzarNewPlayer()}
-
+        val jugador = findViewById<Button>(R.id.button1)
+        jugador.setOnClickListener { lanzarNewPlayer() }
     }
 
-    fun lanzarNewPlayer(){
-        val i=Intent(this,NewPlayer::class.java)
+    private fun lanzarNewPlayer() {
+        val i = Intent(this, NewPlayer::class.java)
         startActivity(i)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.action_search) {
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show()
+            // Acción a realizar cuando se hace clic en "action_search"
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
 }
