49f88f75e5b527848e39128556676e40bda372de
Nebot774 <adriclemenbot@gmail.com>
2023-11-03 19:49:21 +0100

    Practica 3 -3 añadido el floating button

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,7 @@ android {
 
 dependencies {
 
-
+    implementation ("com.google.android.material:material:<latest_version>")
     implementation("com.google.android.material:material:1.8.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
